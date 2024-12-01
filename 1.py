ae9785d36c779b851282a5a58cd71326e68e9fb0
Cre4tos <creatos.yt@gmail.com>
2024-12-01 07:41:32 +0100

    day1 part1

--- 1.py
+++ 1.py
@@ -1,3 +1,22 @@
 
 def part1():
-    pass
+    with open("./inputs/1.txt", "r")as f:
+        lines = f.readlines()
+        list1 = []
+        list2 = []
+        for line in lines:
+            twonums = line.strip().split("   ")
+            list1.append(int(twonums[0]))
+            list2.append(int(twonums[1]))
+        list1.sort()
+        list2.sort()
+        totaldist = 0
+        for num1, num2 in zip(list1, list2):
+            totaldist += abs(num1-num2)
+        print(totaldist)
+
+def main():
+    part1()
+
+if __name__ == '__main__':
+    main()
