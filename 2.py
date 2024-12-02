0c3f1f91c4c47631ea1c4677030df60b24732b3b
Cre4tos <creatos.yt@gmail.com>
2024-12-02 10:01:52 +0100

    day2 part1 done

--- 2.py
+++ 2.py
@@ -4,24 +4,19 @@ def part1():
     lines = getlines(2)
     safe = 0
     for line in lines:
-        print(line)
         cont = False
         changes = []
         numbers = [int(x.strip()) for x in str.split(line," ")]
         for i in range(len(numbers) - 1):
             change = numbers[i] - numbers[i + 1]
-            print(change)
             changes.append(change)
             if abs(change) < 1 or abs(change) > 3:
-                print("to big change")
                 cont = True
         for i in range(len(changes) - 1):
-            if changes[i] > 0 != changes[i + 1] > 0:
-                print("switching")
+            if (changes[i] > 0) != (changes[i + 1] > 0):
                 cont = True
         if cont == True:
             continue
-        print("safe")
         safe += 1
     print(safe)
 
